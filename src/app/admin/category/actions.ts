64b7691869d49fee5589bdbc2c8e4f4a4af40dc5
ben <me@benspace.xyz>
2024-08-19 14:04:35 +0700

    feat: Add CRUD stage

--- src/app/admin/category/actions.ts
+++ src/app/admin/category/actions.ts
@@ -105,6 +105,7 @@ export async function deleteCategory(
 
     await removeCategory({ id });
 
+    revalidatePath("/admin/category");
     return { success: true, message: "Berhasil menghapus Category!" };
   } catch (error) {
     console.log(error);
--- src/types/relation.ts
+++ src/types/relation.ts
@@ -2,3 +2,7 @@ import { Prisma } from "@prisma/client";
 
 export type competitionCategoryWithCompetition =
   Prisma.competition_categoryGetPayload<{ include: { competition: true } }>;
+
+export type stageWithCompetitionCategory = Prisma.stageGetPayload<{
+  include: { competitionCategory: true };
+}>;
