a8367650060bea285cb52c81d4ec04b288f23320
Ahsan <ahsanaz461@gmail.com>
2024-08-20 08:53:38 +0700

    feat: Complete timeline section

--- src/types/relation.ts
+++ src/types/relation.ts
@@ -3,8 +3,11 @@ import { Prisma } from "@prisma/client";
 export type competitionCategoryWithCompetition =
   Prisma.competition_categoryGetPayload<{ include: { competition: true } }>;
 
-export type competitionWithCategoriesAndBatches = Prisma.competitionGetPayload<{
-  include: {
-    competitionCategories: { include: { registrationBatches: true } };
-  };
-}>;
+export type competitionWithCategoriesAndBatchesAndStages =
+  Prisma.competitionGetPayload<{
+    include: {
+      competitionCategories: {
+        include: { registrationBatches: true; stages: true };
+      };
+    };
+  }>;
